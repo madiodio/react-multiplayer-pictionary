ea9de67f363e1b5a3a1730f915f0ea3baae2f0fd
Madiodio Gaye <papigaye1@gmail.com>
2024-02-09 12:26:40 +0000

    move frontend so separate branch

--- backend/constants/maxPoints.js
+++
@@ -1,4 +0,0 @@
-const MAX_POINTS = 300
-const POINTS_MULTIPLIER = 2
-
-module.exports = { MAX_POINTS, POINTS_MULTIPLIER }
--- backend/database/index.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-
-mongoose.connect(`${process.env.MONGODB_CLUSTER}`, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-}, () => {
-    console.log("connected to mongoDB")
-})
